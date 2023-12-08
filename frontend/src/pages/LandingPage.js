8c34a94c26b01325e1cce2a488ae5a7aa2655a06
Amantsehay <130090452+Amantsehay@users.noreply.github.com>
2023-12-08 15:39:44 +0300

    initial setup of the project structure

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RegistrationPage } from '../components/registration/RegistrationPage'
+
+export const LandingPage = () => {
+  return (
+    <div className='landing-page-header'>
+       <RegistrationPage/>
+       Hello Mate do what you want to do
+    </div>
+  )
+}
