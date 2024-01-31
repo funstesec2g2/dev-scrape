31812ae14115b227e69f9e65eb01c760ae9bea66
Bikila Tariku <=>
2024-01-31 19:11:25 +0300

    e2e updated

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -15,10 +15,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('/ (GET)', () => {
+    it('should return "Hello World!"', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
   });
 });
